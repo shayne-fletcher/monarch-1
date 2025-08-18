2d551d04aed2b84faf2c046a948c61dcebf362dc
Marius Eriksen <meriksen@meta.com>
2025-08-18 13:50:36 -0700

    new crate: multipart codec for serde (#831)
    Summary:
    
    Serde codec for multipart messages.
    
    //! Using [`serialize`] / [`deserialize`], fields typed [`Part`] are extracted
    //! from the main payload and appended to a list of `parts`. Each part is backed by
    //! [`bytes::Bytes`] for cheap, zero-copy sharing.
    //!
    //! On decode, the body and its parts are reassembled into the original value
    //! without copying.
    //!
    //! The on-the-wire form is a [`Message`] (body + parts). Your transport sends
    //! and receives [`Message`]s; the codec reconstructs the value, enabling
    //! efficient network I/O without compacting data into a single buffer.
    //!
    //! Implementation note: this crate uses Rust's min_specialization feature to enable
    //! the use of [`Part`]s with any Serde serializer or deserializer. This feature
    //! is fairly restrictive, and thus the API offered by [`serialize`] / [`deserialize`]
    //! is not customizable. If customization is needed, you need to add specialization
    //! implementations for these codecs. See [`part::PartSerializer`] and [`part::PartDeserializer`]
    //! for details.
    
    This will be the foundation for doing zero-copy I/O through our networking stack. With serde_multipart, we can avoid copying byte buffers, and delegate their efficient transmission to the network layer.
    
    The representation -- a multipart message comprising a body and zero or more contiguous byte buffers -- lends itself to other optimizations as well. For example, we could perform transparent RDMA for large parts, and even control the allocation of the received bytes (through the Bytes data type).
    
    The implementation relies on Rust's "min_specialization" feature, which provides a limited form of specialization. This is what allows us to override the serializer for "serde_multipart::Part" when using our multipart-aware encoder. This was quite finnicky to get to work right (the feature accepts only a limited subset of known-to-be-sound specializations), but appears to work well, at the expense of being slightly less generic than we might otherwise have managed.
    ghstack-source-id: 303813996
    exported-using-ghexport
    
    Reviewed By: vidhyav, shayne-fletcher
    
    Differential Revision: D80108937

---
+++ serde_multipart/src/de/bincode.rs
@@ -0,0 +1,409 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::collections::VecDeque;
+use std::io::Read;
+
+use bincode::BincodeRead;
+use bincode::Error;
+use bincode::ErrorKind;
+use bincode::Options;
+use serde::de::IntoDeserializer;
+
+use crate::part::Part;
+
+/// Multipart deserializer for bincode. This passes through to the underlying bincode
+/// deserializer, but dequeues serialized parts when they are needed by [`Part::deserialize`].
+pub struct Deserializer<R, O: Options> {
+    de: bincode::Deserializer<R, O>,
+    parts: VecDeque<Part>,
+}
+
+impl<R, O> Deserializer<R, O>
+where
+    O: Options,
+{
+    pub(crate) fn new(de: bincode::Deserializer<R, O>, parts: VecDeque<Part>) -> Self {
+        Self { de, parts }
+    }
+
+    pub(crate) fn deserialize_part(&mut self) -> Result<Part, Error> {
+        self.parts.pop_front().ok_or_else(|| {
+            ErrorKind::Custom("multipart underrun while decoding".to_string()).into()
+        })
+    }
+
+    pub(crate) fn end(self) -> Result<(), Error> {
+        if self.parts.is_empty() {
+            Ok(())
+        } else {
+            Err(ErrorKind::Custom("multipart overrun while decoding".to_string()).into())
+        }
+    }
+}
+
+// Passthrough to the underlying bincode deserializer; we only override through specialization.
+impl<'de, 'a, R, O> Deserializer<R, O>
+where
+    R: BincodeRead<'de>,
+    O: Options,
+{
+    fn deserialize_len(&mut self) -> Result<usize, Error> {
+        // A visitor that only expects visiting a u64:
+        struct LenVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for LenVisitor {
+            type Value = usize;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a 64-bit length")
+            }
+
+            fn visit_u64<E>(self, value: u64) -> Result<usize, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(value as usize)
+            }
+        }
+
+        use serde::Deserializer;
+        self.de.deserialize_u64(LenVisitor)
+    }
+}
+
+impl<'de, 'a, R, O> serde::Deserializer<'de> for &'a mut Deserializer<R, O>
+where
+    R: BincodeRead<'de>,
+    O: Options,
+{
+    type Error = Error;
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        // Not supported
+        self.de.deserialize_any(visitor)
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_bool(visitor)
+    }
+
+    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_i8(visitor)
+    }
+
+    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_i16(visitor)
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_i32(visitor)
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_i64(visitor)
+    }
+
+    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_u8(visitor)
+    }
+
+    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_u16(visitor)
+    }
+
+    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_u32(visitor)
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_u64(visitor)
+    }
+
+    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_f32(visitor)
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_f64(visitor)
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_char(visitor)
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_str(visitor)
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_string(visitor)
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_bytes(visitor)
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_byte_buf(visitor)
+    }
+
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_unit(visitor)
+    }
+
+    // Below are compound types, which need to recurse into this handler.
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        let value: u8 = serde::de::Deserialize::deserialize(&mut *self)?;
+        match value {
+            0 => visitor.visit_none(),
+            1 => visitor.visit_some(&mut *self),
+            v => Err(ErrorKind::InvalidTagEncoding(v as usize).into()),
+        }
+    }
+
+    fn deserialize_unit_struct<V>(
+        self,
+        name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_unit()
+    }
+
+    fn deserialize_newtype_struct<V>(
+        self,
+        _name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_newtype_struct(self)
+    }
+
+    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        let len = self.deserialize_len()?;
+        self.deserialize_tuple(len, visitor)
+    }
+
+    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        struct Access<'a, R: Read + 'a, O: Options + 'a> {
+            deserializer: &'a mut Deserializer<R, O>,
+            len: usize,
+        }
+
+        impl<'de, 'a, 'b: 'a, R: BincodeRead<'de> + 'b, O: Options> serde::de::SeqAccess<'de>
+            for Access<'a, R, O>
+        {
+            type Error = Error;
+
+            fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, Error>
+            where
+                T: serde::de::DeserializeSeed<'de>,
+            {
+                if self.len > 0 {
+                    self.len -= 1;
+                    let value =
+                        serde::de::DeserializeSeed::deserialize(seed, &mut *self.deserializer)?;
+                    Ok(Some(value))
+                } else {
+                    Ok(None)
+                }
+            }
+
+            fn size_hint(&self) -> Option<usize> {
+                Some(self.len)
+            }
+        }
+
+        visitor.visit_seq(Access {
+            deserializer: self,
+            len,
+        })
+    }
+
+    fn deserialize_tuple_struct<V>(
+        self,
+        _name: &'static str,
+        len: usize,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.deserialize_tuple(len, visitor)
+    }
+
+    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_map(visitor)
+    }
+
+    fn deserialize_struct<V>(
+        self,
+        _name: &'static str,
+        fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.deserialize_tuple(fields.len(), visitor)
+    }
+
+    fn deserialize_enum<V>(
+        self,
+        _name: &'static str,
+        _variants: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        impl<'de, 'a, R: 'a, O> serde::de::EnumAccess<'de> for &'a mut Deserializer<R, O>
+        where
+            R: BincodeRead<'de>,
+            O: Options,
+        {
+            type Error = Error;
+            type Variant = Self;
+
+            fn variant_seed<V>(self, seed: V) -> Result<(V::Value, Self::Variant), Error>
+            where
+                V: serde::de::DeserializeSeed<'de>,
+            {
+                let idx: u32 = serde::de::Deserialize::deserialize(&mut *self)?;
+                let val: Result<_, Error> =
+                    seed.deserialize(IntoDeserializer::into_deserializer(idx));
+                Ok((val?, self))
+            }
+        }
+
+        visitor.visit_enum(self)
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_identifier(visitor)
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        self.de.deserialize_ignored_any(visitor)
+    }
+}
+
+impl<'de, 'a, R, O> serde::de::VariantAccess<'de> for &'a mut Deserializer<R, O>
+where
+    R: BincodeRead<'de>,
+    O: Options,
+{
+    type Error = Error;
+
+    fn unit_variant(self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn newtype_variant_seed<T>(self, seed: T) -> Result<T::Value, Error>
+    where
+        T: serde::de::DeserializeSeed<'de>,
+    {
+        serde::de::DeserializeSeed::deserialize(seed, self)
+    }
+
+    fn tuple_variant<V>(self, len: usize, visitor: V) -> Result<V::Value, Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        serde::de::Deserializer::deserialize_tuple(self, len, visitor)
+    }
+
+    fn struct_variant<V>(
+        self,
+        fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        serde::de::Deserializer::deserialize_tuple(self, fields.len(), visitor)
+    }
+}
