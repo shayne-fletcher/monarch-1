686c7d6f520098c4c7dafd49a1781119e6003711
Peng Zhang <pzhang@meta.com>
2025-08-28 13:32:33 -0700

    Remove a spamming log (#1018)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/1018
    
    It contributes about 85% of logs in the past 12 hours:
    
    https://fburl.com/scuba/monarch_tracing/z7ras86r
    
    Remove it for now. benjipelletier and James Perng are working out a long-term fix.
    
    Reviewed By: benjipelletier
    
    Differential Revision: D81234889
    
    fbshipit-source-id: af9283fc529b1af35a74263aeda987637f27981c

--- hyperactor/src/proc.rs
+++ hyperactor/src/proc.rs
@@ -880,15 +880,7 @@ impl<A: Actor> Instance<A> {
     /// Notify subscribers of a change in the actors status and bump counters with the duration which
     /// the last status was active for.
     fn change_status(&self, new: ActorStatus) {
-        // let old = self.status_tx.send_replace(new.clone());
         self.status_tx.send_replace(new.clone());
-        let actor_id_str = self.self_id().to_string();
-        *self.status_span.lock().expect("can't change") = tracing::debug_span!(
-            "actor_status",
-            actor_id = actor_id_str,
-            actor_name = self.self_id().name(),
-            name = new.arm().unwrap_or_default()
-        );
     }
 
     /// This instance's actor ID.
