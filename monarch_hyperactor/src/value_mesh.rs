a0e4ea02e91c9c02b17d464ac6e65d8c7420a65c
Shayne Fletcher <shaynefletcher@meta.com>
2025-09-11 07:52:09 -0700

    _rust_bindings: expose some more bindings on ValueMesh (#1168)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/1168
    
    this diff extends the python binding for `ValueMesh`.
    adds a constructor from `(Shape, list)`, `__len__`, and `.values()`.
    introduces `From<Shape>` for `Extent` to support the constructor.
    updates the `.pyi` stub and adds basic tests.
    
    Reviewed By: colin2328
    
    Differential Revision: D82147934

--- monarch_hyperactor/src/value_mesh.rs
+++ monarch_hyperactor/src/value_mesh.rs
@@ -7,8 +7,17 @@
  */
 
 use hyperactor_mesh::v1::ValueMesh;
+use ndslice::Extent;
+use ndslice::Region;
+use ndslice::view::BuildFromRegion;
+use ndslice::view::Ranked;
+use ndslice::view::ViewExt;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::types::PyAny;
+use pyo3::types::PyList;
+
+use crate::shape::PyShape;
 
 #[pyclass(
     name = "ValueMesh",
@@ -20,7 +29,35 @@ pub struct PyValueMesh {
 
 #[pymethods]
 impl PyValueMesh {
-    // TODO(SF, 2025-09-10): Implement bindings.
+    /// __init__(self, shape: Shape, values: list)
+    #[new]
+    fn new(_py: Python<'_>, shape: &PyShape, values: Bound<'_, PyList>) -> PyResult<Self> {
+        // Convert shape to region.
+        let extent: Extent = shape.get_inner().clone().into();
+        let region: Region = extent.into();
+        let vals: Vec<Py<PyAny>> = values.extract()?;
+
+        // Build & validate cardinality against region.
+        let inner = <ValueMesh<Py<PyAny>> as BuildFromRegion<Py<PyAny>>>::build_dense(region, vals)
+            .map_err(|e| PyValueError::new_err(e.to_string()))?;
+
+        Ok(Self { inner })
+    }
+
+    /// Return number of ranks (Python: len(vm))
+    fn __len__(&self) -> usize {
+        self.inner.region().num_ranks()
+    }
+
+    /// Return the values in region/iteration order as a Python list.
+    fn values(&self, py: Python<'_>) -> PyResult<PyObject> {
+        // Clone the inner Py objects into a Python list (just bumps
+        // refcounts).
+        let vec: Vec<Py<PyAny>> = self.inner.values().collect();
+        Ok(PyList::new(py, vec)?.into())
+    }
+
+    // TODO(SF, 2025-09-10): Implement more bindings.
 }
 
 pub fn register_python_bindings(module: &Bound<'_, PyModule>) -> PyResult<()> {
