bb1eccba4c7fab878ff02a00401cc96c339e393e
Sam Lurye <slurye@meta.com>
2025-10-31 14:32:50 -0700

    Add bootstrap command hint when proc bootstrap fails (#1719)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/1719
    
    If proc bootstrap (e.g., `monarch/_src/actor/bootstrap_main.py`) fails, provide both the original error message and a hint that indicates that the user might need to look at their proc bootstrap command, and points them to the correct API.
    ghstack-source-id: 320149478
    exported-using-ghexport
    
    Reviewed By: mariusae
    
    Differential Revision: D85903691
    
    fbshipit-source-id: f27d2f36c4351adfc11795ea646a5d9098775e5e

--- python/monarch/_src/actor/bootstrap_main.py
+++ python/monarch/_src/actor/bootstrap_main.py
@@ -37,40 +37,48 @@ async def main() -> None:
 
 
 def invoke_main() -> None:
-    # if this is invoked with the stdout piped somewhere, then print
-    # changes its buffering behavior. So we default to the standard
-    # behavior of std out as if it were a terminal.
-    sys.stdout.reconfigure(line_buffering=True)
-    global bootstrap_main
-
-    # TODO: figure out what from worker_main.py we should reproduce here.
-
-    from monarch._src.actor.telemetry import TracingForwarder  # noqa
-
-    if os.environ.get("MONARCH_ERROR_DURING_BOOTSTRAP_FOR_TESTING") == "1":
-        raise RuntimeError("Error during bootstrap for testing")
-
-    # forward logs to rust tracing. Defaults to on.
-    if os.environ.get("MONARCH_PYTHON_LOG_TRACING", "1") == "1":
-        # we can stream python logs now; no need to forward them to rust processes
-        pass
-        # install opentelemetry tracing
-
     try:
-        with (
-            importlib.resources.as_file(
-                importlib.resources.files("monarch") / "py-spy"
-            ) as pyspy,
-        ):
-            if pyspy.exists():
-                os.environ["PYSPY_BIN"] = str(pyspy)
-            # fallback to using local py-spy
+        # if this is invoked with the stdout piped somewhere, then print
+        # changes its buffering behavior. So we default to the standard
+        # behavior of std out as if it were a terminal.
+        sys.stdout.reconfigure(line_buffering=True)
+        global bootstrap_main
+
+        # TODO: figure out what from worker_main.py we should reproduce here.
+
+        from monarch._src.actor.telemetry import TracingForwarder  # noqa
+
+        if os.environ.get("MONARCH_ERROR_DURING_BOOTSTRAP_FOR_TESTING") == "1":
+            raise RuntimeError("Error during bootstrap for testing")
+
+        # forward logs to rust tracing. Defaults to on.
+        if os.environ.get("MONARCH_PYTHON_LOG_TRACING", "1") == "1":
+            # we can stream python logs now; no need to forward them to rust processes
+            pass
+            # install opentelemetry tracing
+
+        try:
+            with (
+                importlib.resources.as_file(
+                    importlib.resources.files("monarch") / "py-spy"
+                ) as pyspy,
+            ):
+                if pyspy.exists():
+                    os.environ["PYSPY_BIN"] = str(pyspy)
+                # fallback to using local py-spy
+        except Exception as e:
+            logging.warning(f"Failed to set up py-spy: {e}")
+
+        from monarch._src.actor.debugger.breakpoint import remote_breakpointhook
+
+        sys.breakpointhook = remote_breakpointhook
     except Exception as e:
-        logging.warning(f"Failed to set up py-spy: {e}")
-
-    from monarch._src.actor.debugger.breakpoint import remote_breakpointhook
-
-    sys.breakpointhook = remote_breakpointhook
+        bootstrap_err = RuntimeError(
+            f"Failed to bootstrap proc due to: {e}\nMake sure your proc bootstrap command is correct. "
+            f"Provided command:\n{' '.join([sys.executable, *sys.argv])}\nTo specify your proc bootstrap command, use the "
+            f"`bootstrap_cmd` kwarg in `monarch.actor.HostMesh.allocate_nonblocking(...)`."
+        )
+        raise bootstrap_err from e
 
     # Start an event loop for PythonActors to use.
     asyncio.run(main())
