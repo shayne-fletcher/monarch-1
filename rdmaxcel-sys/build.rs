4ba6f386afbdbca8c5e2572fbf97409f7e3e862b
generatedunixname89002005320881 <generatedunixname89002005320881@meta.com>
2025-12-10 15:45:56 -0800

    Fix clippy::needless_borrows_for_generic_args issues in fbcode/monarch/rdmaxcel-sys (#2104)
    Summary: Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/2104
    
    Reviewed By: dtolnay
    
    Differential Revision: D88826481
    
    fbshipit-source-id: 1739bc04b5513ce812b7de18552335a1fb58e8ad

--- rdmaxcel-sys/build.rs
+++ rdmaxcel-sys/build.rs
@@ -264,7 +264,7 @@ fn main() {
 
                 // Use nvcc to compile the CUDA file
                 let nvcc_output = std::process::Command::new(&nvcc_path)
-                    .args(&[
+                    .args([
                         "-c",
                         &cuda_source_path,
                         "-o",
@@ -298,7 +298,7 @@ fn main() {
 
                 // Create static library from the compiled CUDA object
                 let ar_output = std::process::Command::new("ar")
-                    .args(&["rcs", &cuda_lib_path, &cuda_obj_path])
+                    .args(["rcs", &cuda_lib_path, &cuda_obj_path])
                     .output();
 
                 match ar_output {
