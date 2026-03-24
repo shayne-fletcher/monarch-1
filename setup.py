96dad6897d519f8ae116f3e13a6686e797f4b7b9
Riley Dulin <dulinr@meta.com>
2026-03-24 14:17:32 -0700

    Bump nightlies to 0.5.0 and upgrade pytorch (#3183)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/3183
    
    Upgrade our nightlies from 0.4.0 -> 0.5.0 in preparation for the stable release of 0.4.0.
    Also, take this time to upgrade the pytorch version to the latest stable 2.11 before the
    release.
    After this:
    * 0.4.0 stable depends on and builds with pytorch 2.11. ghcr.io/meta-pytorch/monarch:latest will be based off pytorch 2.11's docker image
    * 0.5.0.dev* depends on and builds with pytorch 2.12. ghch.io/meta-pytorch/monarch-nightly:latest will be based off pytorch 2.12's docker image
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D97985377
    
    fbshipit-source-id: 1e9a2cb3647fb4cf3d75b755c44667c65583e540

--- setup.py
+++ setup.py
@@ -465,7 +465,7 @@ class BuildPyWithFrontend(build_py):
 
 # Actual Setup
 package_name = os.environ.get("MONARCH_PACKAGE_NAME", "torchmonarch")
-package_version = os.environ.get("MONARCH_VERSION", "0.4.0.dev0")
+package_version = os.environ.get("MONARCH_VERSION", "0.5.0.dev0")
 
 setup(
     name=package_name,
